b6f17e954353b438eb9c4f05764690c0b8fe0a87
Giuseppe Zerino <g.zerino@f2innovation.com>
2023-11-02 10:18:53 +0100

    Edits for pageview fetcher

--- src/GA4TrafficData.php
+++ src/GA4TrafficData.php
@@ -10,13 +10,13 @@ class GA4TrafficData extends GA4Client {
 
     use DataJoin;
 
-    protected function getDimensionsMap( $dimensionHeaders ) {
+    protected function getDimensionsMap($dimensionHeaders) {
 
         $dimensions = [];
 
         foreach ($dimensionHeaders as $idx => $dimensionHeader) {
 
-            $dimensions[ str_replace('customEvent:bmaff_page_', '', $dimensionHeader->getName() ) ] = $idx;    
+            $dimensions[str_replace('customEvent:bmaff_page_', '', $dimensionHeader->getName())] = $idx;
         }
 
         return $dimensions;
@@ -31,46 +31,65 @@ class GA4TrafficData extends GA4Client {
      * @param string $propertyId
      * @return array
      */
-    public function getPageViews( $propertyId = '295858603', $date = 'yesterday' ) {
-
+    public function getPageViews($propertyId = '295858603', $date = 'yesterday') {
         $pageViewDimensions = [
-            new Dimension([ 'name' => 'Date' ]),
-            new Dimension([ 'name' => 'pagePath' ]),            
-            new Dimension([ 'name' => 'customEvent:bmaff_page_domain'    ]),
-            new Dimension([ 'name' => 'customEvent:bmaff_page_postid'    ]),
-            new Dimension([ 'name' => 'customEvent:bmaff_page_author'    ]), 
-            new Dimension([ 'name' => 'customEvent:bmaff_page_alias'     ]),            
-            new Dimension([ 'name' => 'customEvent:bmaff_page_type'      ]),            
-            new Dimension([ 'name' => 'customEvent:bmaff_page_custom'    ])            
+            new Dimension(['name' => 'Date']),
+            new Dimension(['name' => 'customEvent:bmaff_page_postid']),
+            new Dimension(['name' => 'customEvent:bmaff_page_domain']),
         ];
 
-        $viewRows = $this->getData( $propertyId,  $date,  $pageViewDimensions, 'page_view' ) ;
+        $viewChunkedDimensions = [
+
+            array_merge($pageViewDimensions, [
+                new Dimension(['name' => 'pagepath']),
+                new Dimension(['name' => 'customEvent:bmaff_page_alias']),
+                new Dimension(['name' => 'customEvent:bmaff_page_author']),
+                new Dimension(['name' => 'customEvent:bmaff_page_custom']),
+            ]),
+
+            array_merge($pageViewDimensions, [
+                new Dimension(['name' => 'customEvent:bmaff_page_programs']),
+                new Dimension(['name' => 'customEvent:bmaff_page_subjects']),
+                new Dimension(['name' => 'customEvent:bmaff_page_type']),
+            ]),
+
+        ];
+
+
+        $viewRowsPartials = array_map(fn ($dimensions) => $this->getData($propertyId, $date, $dimensions, 'page_view'), $viewChunkedDimensions);
+
+        $viewRows = array_shift($viewRowsPartials);
+        $viewRows = array_reduce(
+            $viewRowsPartials,
+            fn ($rows, $partial) => $this->leftJoin($rows, $partial, ['Date', 'postid', 'domain'], ['programs' => '', 'subjects' => '', 'type' => '', 'custom' => '']),
+            $viewRows
+        );
 
         /// questa parte si potrà anche rimuovere una volta stabilizzati i postid
-        $viewRows = $this->checkPostIds( $viewRows );
+        $viewRows = $this->checkPostIds($viewRows);
 
-        return $viewRows;        
+        return $viewRows;
     }
 
-    
-    
-    protected function checkPostIds( $rows ) {
 
-        $filteredRows = $this->indexOn( $rows,  ['Date', 'page_view', 'pagePath' ] );
-        
-        $indexedRows  = $this->indexOn( $rows,  ['Date', 'page_view', 'pagePath', 'postid' ] );
 
-        foreach( array_keys( $filteredRows ) as $filteredIndex ) {
+    protected function checkPostIds($rows) {
 
-            foreach( $indexedRows as $index => $row ) {
+        $filteredRows = $this->indexOn($rows,  ['Date', 'page_view', 'pagepath']);
 
-                if( str_contains( $filteredIndex, $index ) ) {
+        $indexedRows  = $this->indexOn($rows,  ['Date', 'page_view', 'pagepath', 'postid']);
 
-                    if( !in_array( $row['postid'], [ 0, '', '(not set)' ] ) ) {
+        foreach (array_keys($filteredRows) as $filteredIndex) {
 
-                        $filteredRows[ $filteredIndex ] = $row;
+            foreach ($indexedRows as $index => $row) {
+
+                if (str_contains($filteredIndex, $index)) {
+
+                    if (!in_array($row['postid'], [0, '', '(not set)'])) {
+
+                        $filteredRows[$filteredIndex] = $row;
                     };
-                }                
+                }
             }
         }
 
@@ -89,18 +108,18 @@ class GA4TrafficData extends GA4Client {
      *
      * @return array
      */
-    function getMostViewedPages( $number = 10, $start_date = '2daysAgo', $end_date = 'today', $threshold = 2000 ) {
-        
+    function getMostViewedPages($number = 10, $start_date = '2daysAgo', $end_date = 'today', $threshold = 2000) {
+
         // Make an API call.
         // https://developers.google.com/analytics/devguides/reporting/data/v1/api-schema?hl=en#dimensions
 
         $response = $this->client->runReport([
-            
+
             'property' => 'properties/' . $this->propertyId,
-            
-            'dateRanges' => [ new DateRange([ 'start_date' => $start_date, 'end_date' => $end_date ]) ],
-            'dimensions' => [ new Dimension([ 'name' => 'landingPage' ]) ],
-            'metrics'    => [ new Metric([ 'name' => 'totalUsers' ]) ],
+
+            'dateRanges' => [new DateRange(['start_date' => $start_date, 'end_date' => $end_date])],
+            'dimensions' => [new Dimension(['name' => 'landingPage'])],
+            'metrics'    => [new Metric(['name' => 'totalUsers'])],
             'limit'      => $number
 
         ]);
@@ -108,25 +127,25 @@ class GA4TrafficData extends GA4Client {
         $mostViewed = [];
 
         /// getRows() è una sorta di iterator
-        foreach( $response->getRows() as $row ) {
+        foreach ($response->getRows() as $row) {
 
             /// la posizione [ 0 ] in dimesions è landingPage
-            $path = $row->getDimensionValues() [ 0 ]->getValue();
-            
+            $path = $row->getDimensionValues()[0]->getValue();
+
             /// prende le parti del pathname dell'articolo ( esclude quelle vuote )
-            $path_parts = array_filter( explode( '/', $path ), fn( $part )  => !empty($part) );
+            $path_parts = array_filter(explode('/', $path), fn ($part)  => !empty($part));
 
             /// prende lo slug
-            $slug = array_pop( $path_parts );
+            $slug = array_pop($path_parts);
 
             /// totalUsers è la metrica [ 0 ] 
-            $totalUsers = $row->getMetricValues()[ 0 ]->getValue(); 
+            $totalUsers = $row->getMetricValues()[0]->getValue();
 
             /// se trova utenti  sopra la soglia desiderata
-            if( $totalUsers > $threshold ) {
+            if ($totalUsers > $threshold) {
 
                 /// ... aggiunge l'articolo con il numero di utenti
-                $mostViewed[] = ( object ) [
+                $mostViewed[] = (object) [
 
                     'slug'  => $slug,
                     'users' => $totalUsers,
@@ -161,24 +180,24 @@ class GA4TrafficData extends GA4Client {
      * @param string $end_date
      * @return object
      */
-    function getUsers( $slug, $start_date = '2daysAgo', $end_date = 'today' ) {
+    function getUsers($slug, $start_date = '2daysAgo', $end_date = 'today') {
 
         // Make an API call.
         // https://developers.google.com/analytics/devguides/reporting/data/v1/api-schema?hl=en#dimensions
 
         $response = $this->client->runReport([
-            
+
             'property' => 'properties/' . $this->propertyId,
-            
-            'dateRanges' => [ new DateRange([ 'start_date' => $start_date, 'end_date' => $end_date ]) ],
-            'dimensions' => [ new Dimension([ 'name' => 'landingPage' ]) ],
-            'metrics'    => [ new Metric([ 'name' => 'totalUsers' ]) ],
+
+            'dateRanges' => [new DateRange(['start_date' => $start_date, 'end_date' => $end_date])],
+            'dimensions' => [new Dimension(['name' => 'landingPage'])],
+            'metrics'    => [new Metric(['name' => 'totalUsers'])],
             'limit' => 1,
             'dimensionFilter' => new FilterExpression([
 
-                'filter' => new Filter([ 
-                    'field_name' => 'landingPage', 
-                    'string_filter'=> new StringFilter( [ 
+                'filter' => new Filter([
+                    'field_name' => 'landingPage',
+                    'string_filter' => new StringFilter([
                         'value' => $slug,
                         'match_type' => MatchType::CONTAINS,
                         'case_sensitive' => false
@@ -190,25 +209,24 @@ class GA4TrafficData extends GA4Client {
         $rows = $response->getRows();
 
 
-        if( !isset( $rows[ 0 ] ) ) return (object) [
+        if (!isset($rows[0])) return (object) [
 
             'slug'  => $slug,
             'users' => 0
         ];
 
-        $row = $rows[ 0 ];
-        
+        $row = $rows[0];
+
         /// la posizione [ 0 ] è landingPage
-        $path = $row->getDimensionValues() [ 0 ]->getValue();
+        $path = $row->getDimensionValues()[0]->getValue();
 
         /// totalUsers è la metrica [ 0 ] 
-        $totalUsers = $row->getMetricValues()[ 0 ]->getValue(); 
+        $totalUsers = $row->getMetricValues()[0]->getValue();
 
         return (object) [
 
             'slug'  => $path,
-            'users' => $totalUsers 
+            'users' => $totalUsers
         ];
-
     }
 }
