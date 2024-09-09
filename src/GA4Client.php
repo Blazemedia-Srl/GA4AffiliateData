d458633415e8767b0008b5ed00b4296b733b1c40
Alessio Carrafa <alessio.carrafa@hotmail.it>
2024-09-09 12:28:05 +0200

    feat: added interval version of getData

--- src/GA4Client.php
+++ src/GA4Client.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace Blazemedia\Ga4AffiliateData;
 
@@ -10,15 +10,16 @@ use Google\Analytics\Data\V1beta\Filter\StringFilter;
 use Google\Analytics\Data\V1beta\BetaAnalyticsDataClient;
 
 
-class GA4Client {
+class GA4Client
+{
 
     protected $client;
 
-    function __construct( protected string $keyPath ) {
-
+    function __construct(protected string $keyPath)
+    {
         $this->client = $this->getClient();
     }
-    
+
 
     /**
      * Make an API call.
@@ -26,20 +27,20 @@ class GA4Client {
      * 
      * @param array $args
      */
-    public function runReport( array $args ) {
-
+    public function runReport(array $args)
+    {
         $default_args =  [
-            
+
             'property' => 'properties/295858603', // TEST OMNIA GA4
-            
-            'dateRanges' => [ new DateRange([ 'start_date' => 'yesterday', 'end_date' => 'yesterday' ]) ],
 
-            'metrics'   => [ new Metric([ 'name' => 'eventCount' ]) ],
+            'dateRanges' => [new DateRange(['start_date' => 'yesterday', 'end_date' => 'yesterday'])],
 
-            'dimensionFilter' => new FilterExpression( [
+            'metrics'   => [new Metric(['name' => 'eventCount'])],
+
+            'dimensionFilter' => new FilterExpression([
                 'filter' => new Filter([
                     'field_name'    => 'eventName',
-                    'string_filter' => new StringFilter( [ 'value' => 'Page View'] )
+                    'string_filter' => new StringFilter(['value' => 'Page View'])
                 ])
             ]),
 
@@ -47,101 +48,108 @@ class GA4Client {
 
         ];
 
-        return $this->client->runReport( array_merge( $default_args, $args ) );
+        return $this->client->runReport(array_merge($default_args, $args));
     }
 
-    
+
     /**
      * Istanzia un client per GA4
      *
      * @return BetaAnalyticsDataClient client
      */
-    protected function getClient() {
+    protected function getClient()
+    {
+        $credentials = file_get_contents($this->keyPath);
 
-        $credentials = file_get_contents( $this->keyPath );
-
-        if( empty( $credentials ) ) throw new \ErrorException( 'Credentials file not found' );
+        if (empty($credentials)) throw new \ErrorException('Credentials file not found');
 
         return new BetaAnalyticsDataClient([
-            'credentials' => json_decode( $credentials, true )
+            'credentials' => json_decode($credentials, true)
         ]);
     }
 
     /**
-     * Ritorna i dati di un certo evento
+     * Ritorna i dati di un certo evento in un intervallo di tempo
      *
-     * @param string $propertyId - stream GA4 da cui estrarre i dati
-     * @param string $eventName  - nome dell'evento
-     * @param array  $dimensions  - campi da estrarre ( array di max 9 string )
-     * @param string $date       - data da considerare
+     * @param string $propertyId    - stream GA4 da cui estrarre i dati
+     * @param string $eventName     - nome dell'evento
+     * @param array  $dimensions    - campi da estrarre ( array di max 9 string )
+     * @param string $startDate     - data di inizio intervallo
+     * @param string $endDate       - data di fine intervallo
      * @return array
      */
-    public function getData( string $propertyId, $date = 'yesterday', array $dimensions = [], string $eventName = '') {
-
+    public function getIntervalData(string $propertyId, $startDate = 'yesterday', $endDate = 'today', array $dimensions = [], string $eventName = '')
+    {
         $params = [
-
             'property' => "properties/{$propertyId}",
-            
-            'dateRanges' => [ new DateRange([ 'start_date' => $date, 'end_date' => $date ]) ],
-
-            'metrics'    => [ new Metric([ 'name' => 'eventCount' ]) ],
-            
+            'dateRanges' => [new DateRange(['start_date' => $startDate, 'end_date' => $endDate])],
+            'metrics'    => [new Metric(['name' => 'eventCount'])],
             'dimensions' => $dimensions,
-            
             'limit' => 100000
         ];
-            
-        if( $eventName != '' ) {
 
-            $params[ 'dimensionFilter' ] = new FilterExpression( [
+        if ($eventName != '') {
+            $params['dimensionFilter'] = new FilterExpression([
                 'filter' => new Filter([
                     'field_name'    => 'eventName',
-                    'string_filter' => new StringFilter( [ 'value' => $eventName ] )
+                    'string_filter' => new StringFilter(['value' => $eventName])
                 ])
             ]);
         }
 
         /// prende i dati da GA4
-        $response = $this->client->runReport( $params );
+        $response = $this->client->runReport($params);
 
         /// prende i nomi delle colonne ( le ripulisce da customEvent:data_bmaff_ ) e li associa agli indici
         /// è un elenco di  [ nome_colonna => indice ]
-        $dimensions = $this->getDimensionsMap( $response->getDimensionHeaders() );
-        
+        $dimensions = $this->getDimensionsMap($response->getDimensionHeaders());
+
         /// cicla le righe
         $rows = [];
-        foreach( $response->getRows() as $row ) {
+        foreach ($response->getRows() as $row) {
 
-            $rows[] = array_reduce( array_keys( $dimensions ), function( $dataRow, $dimension ) use( $row, $dimensions ) {
- 
+            $rows[] = array_reduce(array_keys($dimensions), function ($dataRow, $dimension) use ($row, $dimensions) {
+
                 /// assengna ogni valore alla dimensione corrispondente
-                $dataRow[ $dimension ] = $row->getDimensionValues()[ $dimensions[ $dimension ] ]->getValue();
+                $dataRow[$dimension] = $row->getDimensionValues()[$dimensions[$dimension]]->getValue();
 
                 /// e restituisce la riga
                 return $dataRow;
-
-            }, [ $this->getEventMap( $eventName ) => $row->getMetricValues()[0]->getValue() ] );
+            }, [$this->getEventMap($eventName) => $row->getMetricValues()[0]->getValue()]);
         }
 
         return $rows;
     }
 
+    /**
+     * Ritorna i dati di un certo evento
+     *
+     * @param string $propertyId    - stream GA4 da cui estrarre i dati
+     * @param string $eventName     - nome dell'evento
+     * @param array  $dimensions    - campi da estrarre ( array di max 9 string )
+     * @param string $date          - data da considerare
+     * @return array
+     */
+    public function getData(string $propertyId, $date = 'yesterday', array $dimensions = [], string $eventName = '')
+    {
+        return $this->getIntervalData($propertyId, $date, $date, $dimensions, $eventName);
+    }
 
-    protected function getDimensionsMap( $dimensionHeaders ) {
 
+    protected function getDimensionsMap($dimensionHeaders)
+    {
         $dimensions = [];
 
         foreach ($dimensionHeaders as $idx => $dimensionHeader) {
 
-            $dimensions[ $dimensionHeader->getName() ] = $idx;    
+            $dimensions[$dimensionHeader->getName()] = $idx;
         }
 
         return $dimensions;
     }
 
-    protected function getEventMap( $event ) {
-
-        return str_replace( [],[], $event);
+    protected function getEventMap($event)
+    {
+        return str_replace([], [], $event);
     }
-    
 }
